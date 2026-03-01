85927d32be6ce4aa88c3f7be19c3e586fd5ea5fa
Priyanshi Singh <75989898+priyanships31@users.noreply.github.com>
2026-03-01 19:03:02 +0530

    first commit

---
+++ deploy.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+PROJECT_ID="gcp-vm-autoscaling-security"
+REGION="us-central1"
+
+# Set project
+gcloud config set project $PROJECT_ID
+
+# Create Instance Template
+gcloud compute instance-templates create compute-template-v1 \
+    --machine-type=e2-medium \
+    --image-family=debian-12 \
+    --image-project=debian-cloud \
+    --boot-disk-size=10GB \
+    --tags=http-server,ssh-access
+
+# Create Managed Instance Group
+gcloud compute instance-groups managed create gcp-web-mig \
+    --base-instance-name=gcp-web-mig \
+    --template=compute-template-v1 \
+    --size=1 \
+    --region=$REGION
+
+# Enable Autoscaling
+gcloud compute instance-groups managed set-autoscaling gcp-web-mig \
+    --region=$REGION \
+    --min-num-replicas=1 \
+    --max-num-replicas=10 \
+    --target-cpu-utilization=0.6
+
+# Create Firewall Rule
+gcloud compute firewall-rules create allow-http \
+    --allow=tcp:80 \
+    --direction=INGRESS \
+    --priority=1000 \
+    --network=default \
+    --source-ranges=0.0.0.0/0 \
+    --target-tags=http-server
+
+echo "Deployment Complete!"
